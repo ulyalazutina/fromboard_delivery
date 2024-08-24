ccc4aaeb118f7028c462ff1a0d8698247f12b7ce
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-08-24 17:47:35 +0300

    Адаптивная верстка блока "гарантии качества", увеличение фото при нажатии

---
+++ public/script/imageSize.js
@@ -0,0 +1,7 @@
+lightbox.option({
+    'resizeDuration': 300,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true,
+    'albumLabel':"Фотография %1 из %2",
+    'disableScrolling': true,
+})
