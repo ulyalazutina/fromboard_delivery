d1b2d245be89a0e3519ed159996e29c9f3270cc7
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-08-25 14:49:01 +0300

    Ссылки, псевдоэлементы

---
+++ public/script/scroll.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('a[href*="#"]');
+
+links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const blockId = link.getAttribute('href').substring(1);
+
+        document.getElementById(blockId).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
